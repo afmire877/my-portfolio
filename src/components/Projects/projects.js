658bf302f64f30c6b7d677ddb0c7e6c80e9a50ff
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-10-07 20:21:55 +0100

    Added Project and Icon Components

---
+++ src/components/Projects/projects.js
@@ -0,0 +1,30 @@
+import './projects.css';
+import React, { Fragment } from 'react';
+import ProjectPreview from '../project-preview';
+import Heading from '../Common/heading';
+
+
+ 
+
+const Projects = ({ data }) => {
+    return (
+        <Fragment>
+            <Heading title="Projects" />
+            <div className="projects">
+            {data.allProjectsJson.edges.map(({ node: project }) => (
+                <ProjectPreview
+                key={`preview-${project.slug}`}
+                title={project.title}
+                description={project.description}
+                slug={project.slug}
+                imageData={project.image.childImageSharp.fluid}
+                tags={project.tags}
+                />
+            ))}
+            </div>
+        </Fragment>
+    );
+}
+
+export default Projects;
+
