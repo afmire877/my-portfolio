b4314c840761114f9f6a6c6512648538be5eb727
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-11-18 19:01:07 +0000

    created 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Layout from "../components/layout/layout";
+
+export default () => {
+  return (
+    <Layout>
+      <h1>oops!! This page does not exist</h1>
+    </Layout>
+  );
+};
