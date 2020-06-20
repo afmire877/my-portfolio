efd17c32999c3c6e6a84dec6d026a8dde328c3f1
Ahmed Fuad Mire <33981371+afmire877@users.noreply.github.com>
2020-06-20 16:57:47 +0100

    Fixed bug

--- src/components/readMins.js
+++ src/components/readMins.js
@@ -1,20 +1,25 @@
-import React from "react";
-import { faClock } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Styled from "styled-components";
+import React from 'react';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Styled from 'styled-components';
 
 export default ({ time }) => (
-  <Container>
-    <FontAwesomeIcon
-      css={`
-        color: #2634f2;
-        margin-right: 10px;
-        font-size: 13px;
-      `}
-      icon={faClock}
-    />
-    {time > 1 ? `${time} mins read` : `${time} min read`}
-  </Container>
+  <>
+    {!time && null}
+    {time && (
+      <Container>
+        <FontAwesomeIcon
+          css={`
+            color: #2634f2;
+            margin-right: 10px;
+            font-size: 13px;
+          `}
+          icon={faClock}
+        />
+        {time > 1 ? `${time} mins read` : `${time} min read`}
+      </Container>
+    )}
+  </>
 );
 
 const Container = Styled.div`
