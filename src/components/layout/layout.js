e36b9a85a2b25a2d539c0512421ad5c48de2501d
Ahmed Fuad Mire <33981371+afmire877@users.noreply.github.com>
2020-06-20 16:48:34 +0100

    Added SEO

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,14 +1,15 @@
-import "./layout.css";
-import React from "react";
-import NavBar from "../Navbar/navbar";
-
-if (typeof window !== "undefined") {
+import './layout.css';
+import React from 'react';
+import NavBar from '../Navbar/navbar';
+import SEO from '../SEO/SEO';
+if (typeof window !== 'undefined') {
   // eslint-disable-next-line global-require
-  require("smooth-scroll")('a[href*="#"]');
+  require('smooth-scroll')('a[href*="#"]');
 }
 
 const Layout = ({ children, home }) => (
   <React.Fragment>
+    <SEO />
     <NavBar home={home} />
     <main>{children}</main>
   </React.Fragment>
