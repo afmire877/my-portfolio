c7f2560ccfb5661890d84b0ade17e930e83e9b51
Ahmed Mire <afmire877@gmail.com>
2021-03-13 18:25:05 +0000

    added Google Tag Manager

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -12,6 +12,12 @@ const Layout = ({ children, home }) => (
     <SEO />
     <NavBar home={home} />
     <main>{children}</main>
+    {`
+      <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M84LN6W"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+      `}
   </React.Fragment>
 );
 
--- static/custom-sw-code.js
+++ static/custom-sw-code.js
@@ -2,12 +2,24 @@ setTimeout(() => {
   self.registration.showNotification('Hello, world!');
 }, 15000);
 (async function () {
-  let sw = await navigator.serviceWorker.ready;
-  let push = sw.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey:
-      'BCzT3T936FoK3f90BBMl4Vu3dKeUhL_wUuRpmJ_BExHKLKDujXsF6wuUK2Ea7wsMKb7xRVtqANJ468BAg6BoZx4',
-  });
+
+    Notification.requestPermission(result => {
+        if (result === 'granted') {
+        let sw = await navigator.serviceWorker.ready;
+        let push = sw.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey:
+            'BCzT3T936FoK3f90BBMl4Vu3dKeUhL_wUuRpmJ_BExHKLKDujXsF6wuUK2Ea7wsMKb7xRVtqANJ468BAg6BoZx4',
+        });
+        navigator.serviceWorker.ready.then(registration => {
+          registration.showNotification('Vibration Sample', {
+            body: 'Buzz! Buzz!',
+            tag: 'vibration-sample',
+          });
+        });
+      }
+    });
+
 
   console.log(JSON.stringify(push));
 })();
